b9993403e1875f27725dce704b2aa383bf7fee50
nickdpz <ndpastranzamora@gmail.com>
2020-08-02 11:36:37 -0500

    Segir Pared Alta Velocidad

--- SeguimientoDePared/SeguirPared.m
+++ SeguimientoDePared/SeguirPared.m
@@ -1,126 +1,113 @@
 % % Seguimiento de pared
-function [angR]=seguirPared(ranges,sensorx_R,sensory_R,sensorAngle_R,x,y,theta,seguimiento)
-    d=0.5;%distancia
+function [angT]=seguirPared(ranges,sensorx_R,sensory_R,sensorAngle_R,x,y,theta,seguimiento)
+   d=0.5;
+   ranges
     if seguimiento==1
-    % Componentes del sensor 4, izquierdo atras
-    ang4=sensorAngle_R(5);
-    d4=ranges(5);
-    if isnan(d4)==1
-        d4=4.5;
-    end
-    xs4=sensorx_R(5);
-    ys4=sensory_R(5);
-    Rang4=[cosd(ang4) -sind(ang4); sind(ang4) cosd(ang4)];
-    Ss4=[xs4;ys4];
-    xso4=[d4;0];
-    P1=(Rang4*xso4)+Ss4;
-    % Componentes del sensor 2, izquierdo adelante
-    ang2=sensorAngle_R(3);
-    d2=ranges(3);
-    if isnan(d2)==1
-        d2=4.5;
-    end
-    xs2=sensorx_R(3);
-    ys2=sensory_R(3);
-    Rang2=[cosd(ang2) -sind(ang2); sind(ang2) cosd(ang2)];
-    Ss2=[xs2;ys2];
-    xso2=[d2;0];
-    P2=(Rang2*xso2)+Ss2;
-    % Hallamos el vector pared
-    pared=P2-P1;
-    % Normalizamos el vector pared
-    paredn=(transpose(pared))/(norm(pared));
-    paredn=transpose(paredn);
-    % Calculamos el vector perpendicular a la pared
-    pared_p=P1-((transpose(P1))*paredn)*paredn;
-    % Calculamos la direccion del vector pared_p
-    pared_pn=(transpose(pared_p))/(norm(pared_p));
-    pared_pn=transpose(pared_pn);
-     % Calculamos seguir pared
-    seguir_pared_ad=d*paredn+(pared_p-d*pared_pn);
-    % Calculamos el marco de referencia inercial
-    Rang=[cos(theta) -sin(theta); sin(theta) cos(theta)];
-    Ss=[x;y];
-    Usp=(Rang*seguir_pared_ad)+Ss;
-    % Calculo del angulo
-    thetag=atan2(Usp(1),Usp(2))-180;
-    if ranges(1)<0.5 & isnan(d4)==0
-        angR=evitarObstaculos(ranges,sensorAngle_R,x,y,theta);
-        evitar=1;
+     ang4=sensorAngle_R(5);
+       d4=ranges(5);
+       if isnan(d4)==1
+          d4=4.5; 
+       end
+       xs4=sensorx_R(5);
+       ys4=sensory_R(5);
+       Rang4=[cosd(ang4) -sind(ang4); sind(ang4) cosd(ang4)];
+       Ss4=[xs4;ys4];
+       xo4=[d4;0];
+       P1=(Rang4*xo4)+Ss4;
+       
+       ang2=sensorAngle_R(3);
+       d2=ranges(3);
+       if isnan(d2)==1
+          d2=4.5; 
+       end
+       xs2=sensorx_R(3);
+       ys2=sensory_R(3);
+       Rang2=[cosd(ang2) -sind(ang2); sind(ang2) cosd(ang2)];
+       Ss2=[xs2;ys2];
+       xo2=[d2;0];
+       P2=(Rang2*xo2)+Ss2;
+       pared=P2-P1;
+       paredn=pared/norm(pared);
+       %pared_p=P1-((transpose(P1))*paredn)*paredn;
+       pared_p =-(P1-((transpose(P1))*paredn)*paredn);
+       pared_pn=pared_p/(norm(pared_p));
+       seguir_pared_ad=d*paredn+(pared_p-d*pared_pn);
+       angT = (atan2(seguir_pared_ad(2),seguir_pared_ad(1))+theta);
+%        Rang=[cos(theta) -sin(theta); sin(theta) cos(theta)];
+%        Ss=[x;y];
+%        Usp1=(Rang*seguir_pared_ad);
+%        angR1=atan2(Usp1(2),Usp1(1));
+%        Usp2=(Rang*seguir_pared_ad)+Ss;
+%        angR2=atan2(Usp2(2),Usp2(1));
+%        d1=ranges(1);
+%        angT=theta+angR2;
+%        if isnan(d1)==1
+%            d1=4.5;
+%        end
+%        if d1<0.5 || d2<d || d4<d
+%            angR=evitarObstaculos(ranges,sensorAngle_R,x,y,theta);
+%            angT=theta-angR;
+%        end
+%        if d1<d || d2<d || d4<d
+%            angT=angR2;
+%        else
+%            angT=angR1;
+%        end
+       
     else
-        angR=thetag;
-        evitar=2;
+       % Seguimiento por derecha
+      ang4=sensorAngle_R(4);
+       d4=ranges(4);
+       if isnan(d4)==1
+          d4=4.5; 
+       end
+       xs4=sensorx_R(4);
+       ys4=sensory_R(4);
+       Rang4=[cosd(ang4) -sind(ang4); sind(ang4) cosd(ang4)];
+       Ss4=[xs4;ys4];
+       xo4=[d4;0];
+       P1=(Rang4*xo4)+Ss4;
+       
+       ang2=sensorAngle_R(2);
+       d2=ranges(2);
+       if isnan(d2)==1
+          d2=4.5; 
+       end
+       xs2=sensorx_R(2);
+       ys2=sensory_R(2);
+       Rang2=[cosd(ang2) -sind(ang2); sind(ang2) cosd(ang2)];
+       Ss2=[xs2;ys2];
+       xo2=[d2;0];
+       P2=(Rang2*xo2)+Ss2;
+       pared=P2-P1;
+       paredn=pared/norm(pared);
+       pared_p=P1-((transpose(P1))*paredn)*paredn;
+       %pared_p = - (P1-((transpose(P1))*paredn)*paredn);
+       pared_pn=pared_p/(norm(pared_p));
+       seguir_pared_ad=d*paredn+(pared_p-d*pared_pn);
+       angT = (atan2(seguir_pared_ad(2),seguir_pared_ad(1))+theta);
+%        Rang=[cos(theta) -sin(theta); sin(theta) cos(theta)];
+%        Ss=[x;y];
+%        Usp1=(Rang*seguir_pared_ad);
+%        angR1=atan2(Usp1(2),Usp1(1));
+%        Usp2=(Rang*seguir_pared_ad)+Ss;
+%        angR2=atan2(Usp2(2),Usp2(1));
+       d1=ranges(1);
+%        angT=theta+angR2;
+       if isnan(d1)==1
+           d1=4.5;
+       end
+       if d1<0.5 %|| d2<0.2 || d4<0.2
+           angR=evitarObstaculos(ranges,sensorAngle_R,x,y,theta);
+           angT=(90*pi/180)+theta;
+       end
+%        if d1<d || d2<d || d4<d
+%            angT=angR2;
+%        else
+%            angT=angR1;
+%        end
+       
     end
-    if d2<0.3
-        angR=evitarObstaculos(ranges,sensorAngle_R,x,y,theta);
-        evitar=1
-    end
-    if d4<0.3
-        angR=evitarObstaculos(ranges,sensorAngle_R,x,y,theta);
-        evitar=1
-    end
-    else
-% Seguimiento de pared
-
-% function [angR]=SeguirPared(ranges,sensorx_R,sensory_R,sensorAngle_R,x,y,theta)
-    d=1;%distancia
-    theta1=theta*180/pi;
-    % Componentes del sensor 4, izquierdo atras
-    ang4=sensorAngle_R(4);
-    d4=ranges(4);
-    if isnan(d4)==1
-        d4=4.5;
-    end
-    xs4=sensorx_R(4);
-    ys4=sensory_R(4);
-    Rang4=[cosd(ang4) -sind(ang4); sind(ang4) cosd(ang4)];
-    Ss4=[xs4;ys4];
-    xso4=[d4;0];
-    P1=(Rang4*xso4)+Ss4;
-    % Componentes del sensor 2, izquierdo adelante
-    ang2=sensorAngle_R(2);
-    d2=ranges(2);
-    if isnan(d2)==1
-        d2=4.5;
-    end
-    xs2=sensorx_R(2);
-    ys2=sensory_R(2);
-    Rang2=[cosd(ang2) -sind(ang2); sind(ang2) cosd(ang2)];
-    Ss2=[xs2;ys2];
-    xso2=[d2;0];
-    P2=(Rang2*xso2)+Ss2;
-    % Hallamos el vector pared
-    pared=P2-P1;
-    % Normalizamos el vector pared
-    paredn=(transpose(pared))/(norm(pared));
-    paredn=transpose(paredn);
-    % Calculamos el vector perpendicular a la pared
-    pared_p=P1-((transpose(P1))*paredn)*paredn;
-    % Calculamos la direccion del vector pared_p
-    pared_pn=(transpose(pared_p))/(norm(pared_p));
-    pared_pn=transpose(pared_pn);
-     % Calculamos seguir pared
-    seguir_pared_ad=d*paredn+(pared_p-d*pared_pn);
-    % Calculamos el marco de referencia inercial
-    Rang=[cos(theta) -sin(theta); sin(theta) cos(theta)];
-    Ss=[x;y];
-    Usp=(Rang*seguir_pared_ad)+Ss;
-    % Calculo del angulo
-    thetag=atan2(Usp(1),Usp(2))-pi;
-    if ranges(1)<0.5 & isnan(ranges(1))==0
-        angR=evitarObstaculosI(ranges,sensorAngle_R,x,y,theta);
-        evitar=1
-    else
-        angR=thetag;
-        evitar=2
-    end
-    if d2<0.5
-        angR=evitarObstaculosI(ranges,sensorx_R,sensory_R,sensorAngle_R,x,y,theta);
-        evitar=1
-    end
-    if d4<0.5
-        angR=evitarObstaculosI(ranges,sensorx_R,sensory_R,sensorAngle_R,x,y,theta);
-        evitar=1
-    end
-    end
+    angT*180/pi
+%     pause(1);
 end
--- SeguimientoDePared/seguimientoPared.m
+++ SeguimientoDePared/seguimientoPared.m
@@ -1,3 +1,4 @@
+
 clear all
 close all
 clc
@@ -8,7 +9,7 @@ kinematicModel = differentialDriveKinematics;
 kinematicModel.WheelRadius = (65.65/2)*10^-3;% Diametro de 66.5mm
 kinematicModel.TrackWidth = 19.80*10^-3;%Ancho de la rueda de 19.80mm
 kinematicModel.WheelSpeedRange = [-10  10]*2*pi;
-initialState = [6  3  180*pi/180];   % pose => position in [m], and orientation [deg]
+initialState = [1  0.5  0*pi/180];   % pose => position in [m], and orientation [deg]
 %Posicion inicial en (2,2)
 % mapa
 image = imread('../Images/mapa5.png');
@@ -100,7 +101,7 @@ for idx = 1:numel(t)
         wP(idx) = 0;
     else
         rangesAux=mean(ranges);
-        theta_EO(idx) = seguirPared(rangesAux,sensorx_R,sensory_R,sensorAngle_R,x,y,theta,seguimiento);
+        [theta_EO(idx)] = seguirPared(rangesAux,sensorx_R,sensory_R,sensorAngle_R,x,y,theta,seguimiento);
         e_theta(idx) = wrapToPi(theta_EO(idx) - theta);
         wP(idx) = 1*e_theta(idx);
     end
